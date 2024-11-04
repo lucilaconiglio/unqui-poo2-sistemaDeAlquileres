12eae23ffd2436d779c0e1d92086cf9624dc785a
Fernando <mellolefloth@gmail.com>
2024-11-04 20:40:26 -0300

    added ubicacion

--- src/ubicacion/Ubicacion.java
+++ src/ubicacion/Ubicacion.java
@@ -5,56 +5,38 @@ import java.util.List;
 import resenia.Resenia;
 
 public class Ubicacion {
-	private String tipoDeInmbueble;
-	private String superficie;
-	private int capacidad;
-	private List<String> servicios;
-	private List<String> fotos;
-	private List<Resenia> resenias;
-	
-	public Ubicacion(String tipoDeInmbueble, String superficie, int capacidad, List<String> servicios,
-			List<String> fotos, List<Resenia> resenias) {
-		super();
-		this.tipoDeInmbueble = tipoDeInmbueble;
-		this.superficie = superficie;
-		this.capacidad = capacidad;
-		this.servicios = servicios;
-		this.fotos = fotos;
-		this.resenias = resenias;
-	}
-	
-	public void addFoto(String foto) {
-		fotos.add(foto);
-	}
-	
-	public void removeFoto(String foto) {
-		fotos.remove(foto);
-	}
-	
-	public void addServicio(String servicio) {
-		servicios.add(servicio);
-	}
+   private String pais;
+   private String ciudad;
+   private String direccion;
 
-	public void removeServicio(String servicio) {
-		servicios.remove(servicio);
+   public Ubicacion(String pais, String ciudad, String direccion) {
+	super();
+	this.pais = pais;
+	this.ciudad = ciudad;
+	this.direccion = direccion;
+   }
+
+public String getPais() {
+	return pais;
+}
+
+	public void setPais(String pais) {
+		this.pais = pais;
 	}
 	
-	public List<Resenia> getResenias(){
-		return resenias;
+	public String getCiudad() {
+		return ciudad;
 	}
 	
-	public List<Resenia> getResenias(String resenia){
-		return 	resenias.stream().filter(el-> el.equals(resenia)).toList();
+	public void setCiudad(String ciudad) {
+		this.ciudad = ciudad;
 	}
 	
-	public int getPromedioPuntaje() {
-		//TODO: Ver como calcular el promedio
-		return 0; 
+	public String getDireccion() {
+		return direccion;
 	}
 	
-	public int getPromedioPuntaje(String categoria) {
-		//TODO: Ver como calcular el promedio por categoria
-		return 0; 
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
 	}
-	
 }
