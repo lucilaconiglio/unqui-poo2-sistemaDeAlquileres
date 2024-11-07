af40a7f7b40cb69b92adabd84862e64d7e8f7a7d
vgianfranco-git <gianfranco.ventola@gmail.com>
2024-11-07 19:50:15 -0300

    rankeable completo

--- src/user/inquilino/Inquilino.java
+++ src/user/inquilino/Inquilino.java
@@ -2,6 +2,7 @@ package user.inquilino;
 
 import user.User;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,17 +12,23 @@ import reserva.Reserva;
 
 public class Inquilino extends User{
 	private List<Resenia> resenias;
+	private List<Reserva> reservas;
 
 
 	public Inquilino(String nombreCompleto, String mail, int numeroDeTelefono) {
 		super(nombreCompleto, mail, numeroDeTelefono);
 		this.resenias=new ArrayList<Resenia>();
+		this.reservas=new ArrayList<Reserva>();
 
 	}
 
-	public List<Reserva> getReservasFuturas(){
-		return null;
-	}
+	public List<Reserva> getReservasFuturas() {
+        LocalDate hoy = LocalDate.now();
+        
+        // Filtrar reservas que tienen fecha de inicio mayor a hoy
+        return reservas.stream().filter(reserva -> reserva.getFechaInicio().isAfter(hoy)).toList();
+    }
+
 	
 	public List<Reserva> getReservasDeCiudad(String ciudad){
 		return null;
