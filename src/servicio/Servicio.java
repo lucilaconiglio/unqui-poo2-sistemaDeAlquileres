99def215b56e45408f399ac28b53e32f9152ec0c
vgianfranco-git <gianfranco.ventola@gmail.com>
2024-11-08 18:54:24 -0300

    modificamos sitio y creamos clases de: servicio, categoria y tipo de inmueble

---
+++ src/servicio/Servicio.java
@@ -0,0 +1,14 @@
+package servicio;
+
+public class Servicio {
+
+	private String concepto;
+	
+	public Servicio(String concepto) {
+		this.concepto=concepto;
+	}
+	public String getConcepto() {
+		return this.concepto;
+	}
+
+}
